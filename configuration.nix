c76b0a82add54f0aa31ee91d29ad8b1fa246ee22
applyinnovations <alexander@applyinnovations.com.au>
2024-07-08 15:16:36 +1000

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }:
+
+let
+  cicd = pkgs.buildGoModule rec {
+    pname = "cicd";
+    version = "0.0.1";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "applyinnovations";
+      repo = "cicd";
+      rev = "bcabb61d19c85375684fbf7cae8ce4827604a916";
+      sha256 = "sha256-7rARHP+DTdouw/RTN8S798VvyI2ytcjTUMKH4FCcnr4=";
+    };
+
+    vendorHash = null;
+  };
+in
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  users.users.test = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    initialPassword = "test";
+  };
+
+  environment.systemPackages = with pkgs; [
+    docker
+    cicd
+  ];
+
+  systemd.services.cicd = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${cicd}/bin/cicd";
+      Restart = "always";
+    };
+    requires = [ "network.target" ];
+  };
+
+  networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+
+  system.stateVersion = "23.11";
+}
